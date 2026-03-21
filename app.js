7cec4130303efead00ae38b12d424ce73c349d23
Elizaveta877 <ep50384@gmail.com>
2026-03-21 20:18:34 +0200

    Change

--- app.js
+++ app.js
@@ -4,12 +4,14 @@ const path = require('path');
 const jwt = require('jsonwebtoken');
 const SECRET_KEY = 'your_secret_key';
 const app = express();
+const users = [];
+const PORT = 3000;
+
+app.use(express.json());
+app.use (express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
-
-
-app.use(express.json());
 app.use(cookieParser());
 
 
@@ -19,9 +21,37 @@ app.get('/', (req, res) => {
   res.render('index', { theme: currentTheme });
 });
 
+// registration
+app.get('/register', (req, res) => {
+  const { username, password } = req.query;
+  if (!username || !password) {
+    return res.status(400).json({ message: 'Username and password are required' });
+  }
+
+const userExists = users.find(user => user.username === username);
+if (userExists) {
+  return res.status(400).json({ message: 'Username already exists' });
+}
+users.push({ username, password });
+res.status(201).json({ message: 'User registered successfully' });
+});
+
+
+app.get('/login', (req, res) => {
+  const { username, password } = req.query;
+  const user = users.find(user => user.username === username && user.password === password);
+  if (!user) {
+    return res.status(401).json({ message: 'Invalid credentials' });
+  }
+const token = jwt.sign({ username }, SECRET_KEY, { expiresIn: '1h' });
+res.cookie('token', token, { httpOnly: true, secure: false });
+res.json({ message: 'Logged in successfully', username: user.username });
+});
+
+
 app.get('/set-theme/:theme', (req, res) => {
   const { theme } = req.params;
-  res.cookie('theme', theme, {maxAge: 365 * 24 *60 *60 *1000});
+  res.cookie('theme', theme, { maxAge: 365 * 24 * 60 * 60 * 1000 });
   res.redirect('/');
 });
 
@@ -52,6 +82,6 @@ app.get('/dashboard', authenticateJWT, (req, res) => {
   res.json({ message: `Welcome to the dashboard, ${req.user.username}!` });
 });
 
-app.listen(3000, () => {
-  console.log('Server is running on port 3000');
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
 });
